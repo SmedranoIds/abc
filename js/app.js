85d115ac7d1e750a7682af245d0deaa27a1fc771
Matt Foster <aortizv@ids.com.mx>
2017-08-21 17:54:16 -0500

    CAmbio a sprite

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
 $( document ).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip();
-
-	$(".fa-certificate , .fa-cog").hover(function(){
-		$(this).addClass("fa-spin");
-	})
-	$(".fa-certificate , .fa-cog").mouseleave(function(){
-		$(this).removeClass("fa-spin");
-	})
 	$("#checkbox-a").click(function(){
 		seleccion();
 	})
